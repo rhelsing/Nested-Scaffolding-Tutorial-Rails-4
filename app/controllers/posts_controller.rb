ff82f261b3b4bda3834c73fd6bedae582724a964
Ryan Helsing <ryanhelsing@gmail.com>
2014-08-22 14:19:49 -0400

    initial push

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,82 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  # GET /posts.json
+  def index
+    #!!! Specific posts
+    @posts = User.find(params[:user_id]).posts
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    #!!! pass user to form
+    @user = User.find(params[:user_id])
+    @post = Post.new(:user_id => @user.id)#need to specify ahead of time
+  end
+
+  # GET /posts/1/edit
+  def edit
+    #!!! pass user to form
+    @user = User.find(params[:user_id])
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        #!!! correct redirect
+        format.html { redirect_to user_post_path(@post.user.id, @post.id), notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        #!!! correct redirect
+        format.html { redirect_to user_post_path(@post.user.id, @post.id), notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      #!!! correct redirect
+      format.html { redirect_to user_posts_path(@post.user.id), notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
+end
